40d0993e3259926b24c747ab96fad1f8709e7a9b
linjielinlin <993353454@qq.com>
2023-03-01 10:33:01 +0800

    feat: init

---
+++ src/demo-button/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="demo-button" :style="{ backgroundColor: color }">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'DemoButton',
+
+  props: {
+    color: String,
+    type: {
+      type: String,
+      default: 'primary',
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.demo-button {
+  min-width: 120px;
+  color: #fff;
+  font-size: 16px;
+  line-height: 36px;
+  background-color: grey;
+  border: none;
+  border-radius: 30px;
+}
+</style>
