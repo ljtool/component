ae946d5b3818a96be1ffd8e9015b3f04cefa4a04
LinJieLinLin <993353454@qq.com>
2023-05-26 17:33:27 +0800

    feat: add scroll-x directives

--- src/demo-button/index.vue
+++ src/demo-button/index.vue
@@ -18,7 +18,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 .demo-button {
   min-width: 120px;
   color: #fff;
---
+++ src/scroll-x/demo/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <demo-block title="鼠标点击内容移动">
+    <div
+      id="a"
+      v-scroll-x
+      style="height: 100px; width: 100vw; background: grey; overflow: auto"
+    >
+      <div style="width: 200vw; background: grey">test text</div>
+      <div style="width: 200vw; background: grey">
+        test texttest texttest texttest texttest texttest text
+      </div>
+    </div>
+  </demo-block>
+</template>
+
+<script setup>
+import { scrollX as vScrollX } from 'lj-utils/directive/scrollX';
+</script>
