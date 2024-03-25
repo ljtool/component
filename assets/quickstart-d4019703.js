02db80b649807edeb95aa5deae225c30f3ce4f38
LinJieLinLin <993353454@qq.com>
2024-03-25 15:33:39 +0000

    github actions auto commit

---
+++ assets/quickstart-d4019703.js
@@ -0,0 +1,9 @@
+import{o as a,a as s,z as o}from"./vue-libs-d2aebc66.js";const t={class:"van-doc-markdown-body"},e=o(`<h1>快速上手</h1><div class="van-doc-card"><h3 id="an-zhuang" tabindex="-1">安装</h3><pre><code class="language-bash"><span class="hljs-comment"># 通过 npm</span>
+npm i lj-component
+
+<span class="hljs-comment"># 通过 yarn</span>
+yarn add lj-component
+
+<span class="hljs-comment"># 通过 pnpm</span>
+pnpm add lj-component
+</code></pre></div>`,2),c=[e],_={__name:"quickstart",setup(p,{expose:n}){return n({frontmatter:{}}),(m,r)=>(a(),s("div",t,c))}};export{_ as default};
