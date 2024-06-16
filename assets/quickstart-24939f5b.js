529e16aeda6f9ec8e9c5b9db352f1961adb4448c
LinJieLinLin <993353454@qq.com>
2024-06-16 15:45:32 +0000

    github actions auto commit

--- assets/quickstart-24939f5b.js
+++ assets/quickstart-24939f5b.js
@@ -1,4 +1,4 @@
-import{o as a,a as s,z as o}from"./vue-libs-d2aebc66.js";const t={class:"van-doc-markdown-body"},e=o(`<h1>快速上手</h1><div class="van-doc-card"><h3 id="an-zhuang" tabindex="-1">安装</h3><pre><code class="language-bash"><span class="hljs-comment"># 通过 npm</span>
+import{o as a,a as s,z as o}from"./vue-libs-9169e31b.js";const t={class:"van-doc-markdown-body"},e=o(`<h1>快速上手</h1><div class="van-doc-card"><h3 id="an-zhuang" tabindex="-1">安装</h3><pre><code class="language-bash"><span class="hljs-comment"># 通过 npm</span>
 npm i lj-component
 
 <span class="hljs-comment"># 通过 yarn</span>
